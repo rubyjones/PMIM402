4f79e14d8897eac89f6a85d639334dd477a5ff3c
Ruby Jones <rubyjones@Rubys-MacBook.local>
2026-05-13 14:22:50 +0100

    train and test data

---
+++ 1-load-data.r
@@ -0,0 +1,11 @@
+data(mlc_churn)
+
+set.seed(999)
+
+# create train and test data
+bound <- nrow(df)*0.75
+mlc_churn <- mlc_churn[sample(nrow(df)), ]
+churn_train <- mlc_churn[1:bound,]
+churn_test <- mlc_churn[(bound + 1): nrow(mlc_churn), ]
+
+saveRDS(churn_test, "churn_test.rds")
