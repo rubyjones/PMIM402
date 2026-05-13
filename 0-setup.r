bd02c62e474a1fb7a659e7c04577e78bc2723d49
Ruby Jones <rubyjones@Rubys-MacBook.local>
2026-05-13 14:08:12 +0100

    installed r packages

--- 0-setup.r
+++ 0-setup.r
@@ -1,1 +1,8 @@
-install.packages
+install.packages(c("modeldata", "pins", "ranger", "caret", "yardstick"))
+library(modeldata)
+library(pins)
+library(ranger)
+library(caret)
+library(yardstick)
+
+## test
